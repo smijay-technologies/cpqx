9e1133e7b47c25f72eb3125f334dfd4aaab45aba
Nikhil <nikhilb@smijaytechnologies.com>
2024-11-06 13:01:38 +0530

    CPQX-20: UI to show Quote, Quote Line, Products information in one page

--- force-app/main/default/lwc/searchBox/searchBox.js
+++ force-app/main/default/lwc/searchBox/searchBox.js
@@ -3,12 +3,10 @@ import { LightningElement } from 'lwc';
 export default class SearchBox extends LightningElement {
   handleSearchChange(event) {
     // Fire custom event with the search key
-    const searchKey = event.target.value,
+    const search = event.target.value,
       searchEvent = new CustomEvent('search', {
-        detail: { searchKey }
+        detail: { search }
       });
-
-    // Dispatch the custom event
     this.dispatchEvent(searchEvent);
   }
 }
